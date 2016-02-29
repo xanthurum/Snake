f7bd4dbd7c2a5fc48f918c12a6b14881a581f695
Pascal Barbary <pascal.barbary@student.kuleuven.be>
2016-02-29 17:08:30 +0100

    snake commit 1

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -1,10 +1,36 @@
 
-public class Snake {
+public class Snake 
+{
+	public static void main (String[]args)
+	{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		System.out.println("bye bye");
+		public String[][] matrix;
+		public int B, H;
+	
+		public Snake(int breedte, int hoogte) 
+		{
+			matrix = new String[hoogte][breedte];
+			B = breedte;
+			H = hoogte;
+			vullen();
+		}
+	
+		private void vullen()
+		{
+			int i =0, h = 0;
+			while(h < H)
+			{
+			matrix[i][h] = "o";
+			System.out.print(matrix[i][h]);
+			i++;
+				if(i == B)
+				{
+					i = 0;
+					h++;
+					System.out.println();
+				}
+			}
+		}
 	}
 
 }
