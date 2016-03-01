30218aae0fb1afd213a964239d36c597c2377a5e
Pascal Barbary <pascal.barbary@student.kuleuven.be>
2016-03-01 10:54:33 +0100

    snake commit 2

---
+++ Snake/src/Matrix.java
@@ -0,0 +1,41 @@
+
+public class Matrix {
+
+	int H, B;
+	String[][] matrix;
+	
+	public Matrix(int breedte, int hoogte) 
+	{
+		matrix = new String[breedte][hoogte];
+		H = breedte;
+		B = hoogte;
+		vullen();
+	}
+	
+	public String[][] vullen()
+	{
+		System.out.println("test");
+		int i =0, h = 0;
+		while(h < H)
+		{
+		matrix[i][h] = "o";
+		System.out.print(matrix[i][h]);
+		i++;
+			if(i == B)
+			{
+				i = 0;
+				h++;
+				System.out.println();
+			}
+		}
+		return matrix;
+	}
+	public int getH()
+	{
+		return H;
+	}
+	public int getB()
+	{
+		return B;
+	}
+}
--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -1,36 +1,39 @@
 
 public class Snake 
 {
-	public static void main (String[]args)
+	public String[][] matrix;
+	public int B, H;
+	
+	public Snake(int breedte, int hoogte) 
 	{
-
-		public String[][] matrix;
-		public int B, H;
-	
-		public Snake(int breedte, int hoogte) 
-		{
-			matrix = new String[hoogte][breedte];
-			B = breedte;
-			H = hoogte;
-			vullen();
-		}
-	
-		private void vullen()
-		{
-			int i =0, h = 0;
-			while(h < H)
-			{
-			matrix[i][h] = "o";
-			System.out.print(matrix[i][h]);
-			i++;
-				if(i == B)
-				{
-					i = 0;
-					h++;
-					System.out.println();
-				}
-			}
-		}
+		super();
+		B = breedte;
+		H = hoogte;
+		matrix = new Matrix(breedte, hoogte);
 	}
 
+
+	public static void main (String[]args)
+	{
+		/*System.out.println(B);
+		System.out.println(H);
+		int i =0, h = 0;
+		while(h < H)
+		{
+		//matrix[i][h] = "o";
+		System.out.print(matrix[i][h]);
+		i++;
+			if(i == B)
+			{
+				i = 0;
+				h++;
+				System.out.println();
+			}
+		}*/
+	}
 }
+	
+
+	
+
+
